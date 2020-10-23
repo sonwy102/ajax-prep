a727bce833634f7f0d10e73dd2bce2ed75983d93
Wooyang Son <sonwy102@gmail.com>
2020-10-23 04:24:31 +0000

    add jquery to render profile-response

--- static/profile.js
+++ static/profile.js
@@ -5,13 +5,46 @@ function submitProfile(evt) {
   const formData = {
     name: $('#name-field').val(),
     age: $('#age-field').val(),
-    occupation: $('#occupation-field').val()
+    occupation: $('#occupation-field').val(),
+    education: $('#education-field').val(),
+    salary: $('#salary-field').val(),
+    state: $('#state-field').val(),
+    city: $('#city-field input:checked').val(),
+    garden: $('#garden input:checked').val(),
+    tv: $('select[name="tv"]').val()
   };
 
   $.post('/api/profile', formData, (response) => {
-    $('#profiles').append(`
-      <li>${response.fullname} the ${response.occupation} is ${response.age}</li>
+    $('#profile-response').html(`
+      <li>${response.fullname} the ${response.occupation} is ${response.age}.</li>
+      <li>${response.fullname}'s highest education level is ${response.education}. 
+      They earn $${response.salary} a year.</li>
+      <li>${response.fullname} lives in ${response.city} ${response.state}.</li>
     `);
+
+    if (response.garden === 'yes') {
+      $("#profile-response").append(`
+        <li>${response.fullname} likes to garden.</li>
+      `);
+    } else {
+      $("#profile-response").append(`
+        <li>${response.fullname} does not garden.</li>
+      `);
+    }
+
+    if (response.tv === 'none') {
+      $("#profile-response").append(`
+        <li>${response.fullname} does not watch TV.</li>
+      `);
+    } else if (response.tv === 'bingewatcher') {
+      $("#profile-response").append(`
+        <li>${response.fullname} is a bingewatcher when it comes to TV.</li>
+      `);
+    } else {
+      $("#profile-response").append(`
+        <li>${response.fullname} watches ${response.tv} hours of TV.</li>
+      `);
+    }
   });
 }
 
